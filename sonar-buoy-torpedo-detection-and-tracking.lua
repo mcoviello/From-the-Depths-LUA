3cef590382f1eaa8cf7ceb4f98d450f77153182b
Michael Coviello <mcnika1234@gmail.com>
2021-11-04 22:20:10 +0000

    Torpedo Anti-Floor Collision
    Allows for torpedos to check the terrain altitude at enemy craft location, to avoid crashing into the seabed.

--- sonar-buoy-torpedo-detection-and-tracking.lua
+++ sonar-buoy-torpedo-detection-and-tracking.lua
@@ -1,4 +1,5 @@
 --[[
+
 sonar-buoy-torped-detection-and-tracking.lua
 by Michael Coviello
 
@@ -12,35 +13,40 @@ Example Setup:
 
 ]]--
 
-
 function Update(I)
 
-    --Create an array to store the missiles that have a target
+    local TORPEDO_HOVER_DEPTH = 100
+
+    --Create an array to store the missiles that have a target.
     local usedMissiles = {};
     for i = 0, I:GetLuaControlledMissileCount(0) - 1 do
-        usedMissiles[i] = true
+        usedMissiles[i] = false
     end
 
     for t = 0, I:GetNumberOfTargets(0) - 1 do
-        if target.Valid() and target.Position.y <= 0 then
-            local curTarget = I:GetTargetInfo(0,t)
+        local curTarget = I:GetTargetInfo(0,t)
+        --If the target is below sea level...
+        if curTarget.Valid and curTarget.Position.y <= 0 then
             local closestMissileIndex = 0
             local closestDistanceFromTarget = Vector3.Distance(I:GetLuaControlledMissileInfo(0,0).Position, curTarget.Position)
+            --Calculate the closest missile to the target, that isn't already on a target.
             for m = 1, I:GetLuaControlledMissileCount(0) - 1 do
                 local currentMissileDistanceFromTarget = Vector3.Distance(I:GetLuaControlledMissileInfo(0,m).Position, curTarget.Position)
-                if currentMissileDistanceFromTarget < closestDistanceFromTarget and usedMissiles[m] == false then
+                if currentMissileDistanceFromTarget < closestDistanceFromTarget and not usedMissiles[m] then
                     closestMissileIndex = m
                     closestDistanceFromTarget = currentMissileDistanceFromTarget
                 end
             end
+
+            --For the closest missile, make it hover below the sub (or above, if the sub is too deep)
             usedMissiles[closestMissileIndex] = true
             local goal_pos = curTarget.Position
-            goal_pos.y = goal_pos.y - 100
-
+            goal_pos.y = (curTarget.Position.y - TORPEDO_HOVER_DEPTH > I:GetTerrainAltitudeForPosition(curTarget.Position.x, curTarget.Position.y, curTarget.Position.z) and goal_pos.y - TORPEDO_HOVER_DEPTH or goal_pos.y + TORPEDO_HOVER_DEPTH)
             I:SetLuaControlledMissileAimPoint(0, closestMissileIndex, goal_pos.x, goal_pos.y, goal_pos.z)
         end
     end
 
+    --Make all unused missiles continue in their launch direction
     for m = 0, I:GetLuaControlledMissileCount(0) - 1 do
         if usedMissiles[m] == false then
             local current_pos = I:GetLuaControlledMissileInfo(0,m).Position + I:GetLuaControlledMissileInfo(0,m).Velocity
